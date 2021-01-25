aac884db29860b8c023d5048ec02d0477716be07
TamannaJain2710 <73113728+TamannaJain2710@users.noreply.github.com>
2021-01-25 17:38:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -43,7 +43,7 @@ function draw() {
 function keyPressed(){
 	if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:79.15,y:-79.15});
+		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:79.10,y:-79.10});
   
 	
 	  }
